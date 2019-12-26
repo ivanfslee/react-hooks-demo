54244c59eb20215e6916bd4ea2addcfe3e6170fc
ivanfslee <ivanfslee@gmail.com>
2019-12-26 11:22:38 -0800

    Implemeted get request to jsonplaceholder api

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import axios from 'axios';
 
 class ResourceList extends React.Component {
+    state = {
+        resources: []
+    };
+
+    async componentDidMount() {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+        this.setState({ resources: response.data });
+    }
+
     render() {
         return (
             <div>
-                <h1>{this.props.resource}</h1>
+                <h1>{this.state.resources.length}</h1>
             </div>
         );
     }
