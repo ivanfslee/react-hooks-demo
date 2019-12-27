a19130de10ec6ac8e9ea2c816856306c6c047cc3
ivanfslee <ivanfslee@gmail.com>
2019-12-27 14:31:36 -0800

    Refactored hooks code to separate useResources.js file

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,22 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+import useResources from './useResources';
 
 const ResourceList = ({ resource }) => { //destructuring props passed in from parent component App.js - resource = props.resource
-    const [ resources, setResources ] = useState([]); //initialize state to be empty array
-
-    const fetchResource = async (resource) => {
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
-        setResources(response.data); //equivalent to this.setState({resources: response.data})
-    };
-  
-    useEffect(() => {
-        fetchResource(resource);
-    }, [resource]);
+    const resources = useResources(resource);
 
     return (
-        <div>
-            <h1>{resources.length}</h1>
-        </div>
+        <ul>{resources.map(record => <li key={record.id}>{record.title}</li> )}</ul>
     );
 }
 
