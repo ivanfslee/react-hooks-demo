1eaf50e55bef5245e9830c0b32759db2e1897b93
ivanfslee <ivanfslee@gmail.com>
2019-12-28 16:11:19 -0800

    Added UserList component that uses useResources hook to make axios get request

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'; //useState - part of react Hooks - allows functional based components to use state
 import ResourceList from './ResourceList';
+import UserList from './UserList';
 
 const App = () => {
     const [resource, setResource] = useState('posts');
     return (
         <div>
+            <UserList />
             <div>
                 <button onClick={() => setResource('posts')}>Posts</button>
                 <button onClick={() => setResource('todos')}>Todos</button>
